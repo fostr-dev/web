04acb65b65f2484335dd3ad6ac81738ab3a2b6f1
KaffinPX <kaffinpx@gmail.com>
2023-03-18 03:09:56 +0300

    im learning react by killing thomizs code

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,13 +2,37 @@ import { Box } from "@mui/material";
 
 export default function Main(){
     return <Box sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "var(--app-height)",
-        width: "100%",
-        padding: 2
-    }}>
+      minHeight: "var(--app-height)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+
+      ".card-group": {
+          display: "flex",
+          flexDirection: "row",
+          gap: 2,
+          padding: 2,
+          justifyContent: "center",
+          flexWrap: "wrap",
+          width: "100%",
+
+          "& > *": {
+              width: "100%",
+              maxWidth: 600,
+              padding: 2,
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              alignItems: "center",
+              textAlign: "center",
+
+              "& > *": {
+                  width: "100%"
+              }
+          }
+      }
+  }}>
         <TextField
             label="Search for an user"
         />
