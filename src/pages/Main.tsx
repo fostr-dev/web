443bc2e4ed4524f51183ec5e39846798d22fce7a
KaffinPX <kaffinpx@gmail.com>
2023-03-18 03:12:29 +0300

    fix

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,38 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 
 export default function Main(){
     return <Box sx={{
-      minHeight: "var(--app-height)",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "100%",
-
-      ".card-group": {
-          display: "flex",
-          flexDirection: "row",
-          gap: 2,
-          padding: 2,
-          justifyContent: "center",
-          flexWrap: "wrap",
-          width: "100%",
-
-          "& > *": {
-              width: "100%",
-              maxWidth: 600,
-              padding: 2,
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-              alignItems: "center",
-              textAlign: "center",
-
-              "& > *": {
-                  width: "100%"
-              }
-          }
-      }
-  }}>
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "var(--app-height)",
+        width: "40%",
+        padding: 2
+    }}>
         <TextField
             label="Search for an user"
         />
